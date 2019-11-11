3728ce0a3fc4000abfdb1aacdb1c395daa10e340
Duncan R Skilton <MC03353@mbro.ac.uk>
2019-11-11 16:30:44 +0000

    add String check

--- src/scannerinputvalidation/ScannerInputValidation.java
+++ src/scannerinputvalidation/ScannerInputValidation.java
@@ -12,25 +12,39 @@ import java.util.Scanner;
  * @author MC03353
  */
 public class ScannerInputValidation {
-
+    public static final Scanner SC = new Scanner(System.in);//initialise public Scanner and give it the name SC
+    
     public static void main(String[] args) {
-        
-        positiveInt();
+        stringCheck();
+//        positiveInt();
     }
     
     public static void positiveInt(){ //public access, static so we can call it in the main (above), void because we aren't expacting a return type
-        Scanner sc = new Scanner(System.in); //initialise Scanner and give it the name sc
         int num; //declare an int called number
         
-        do {
+        do {// a do while loop will run through the code before performing the logic check
             System.out.print("Enter a positive number: ");
-            while (!sc.hasNextInt()) {//while Scanners input is not a whole number i.e.
+            while (!SC.hasNextInt()) {//while Scanners input is not a whole number i.e.
                 System.out.println("Must be a number");//
-                sc.next(); //you need this otherwise wil not break from loop
+                SC.next();//advances Scanner to next line. hasNext will not advance to next line so program will get caught in loop
             }
-            num = sc.nextInt();
+            num = SC.nextInt();
         } while (num <= 0);//will run the loop unit the number is greater than or equal to 0 
 
         System.out.println("Your positive number is: " + num);
-    }
+    }//end of positiveInt method
+    
+    public static void stringCheck() {//will check for matching Strings
+        System.out.println("Please enter a String");
+        
+        while(!SC.hasNext("word")){
+            System.out.println("String does not match");
+            SC.nextLine();//advances Scanner to next line. hasNext will not advance to next line so program will get caught in loop
+        }//end of while loop
+        
+        String string = SC.nextLine();
+        System.out.println("We have a match " + string);
+    }//end of stringCheck
+    
+    
 }
